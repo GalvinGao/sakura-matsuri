d3e0681972651479b09f6d655efb464abf7c5806
GalvinGao <me@galvingao.com>
2023-03-04 17:33:31 -0500

    feat: add legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import replace from '@rollup/plugin-replace'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
+import legacy from '@vitejs/plugin-legacy'
 import viteRawPlugin from 'vite-raw-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteRawPlugin({ fileRegex: /\.md$/ })],
+  plugins: [
+    react(),
+    viteRawPlugin({ fileRegex: /\.md$/ }),
+    replace({
+      __SENTRY_DEBUG__: false,
+    }),
+    legacy({
+      targets: ['last 5 versions and not dead', 'Firefox ESR', '>= 0.5% in US'],
+    }),
+  ],
 })
