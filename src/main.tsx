1da92a7f4d146e169479c8e7b7ebbaaa604771f1
GalvinGao <me@galvingao.com>
2023-03-04 14:33:46 -0500

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './i18n'
 import './index.css'
+
+Sentry.init({
+  dsn: 'https://f87d6e46289f474ba6428845b1514404@o265165.ingest.sentry.io/4504781995048960',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.05,
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
